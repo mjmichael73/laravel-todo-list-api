9b82e69a1fe4f37e63b50470e5d414e8e4a5a29e
Mojtaba Michael <mojimich2015@gmail.com>
2022-06-29 15:18:59 +0430

    It has been added store method for Todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class TodoController extends Controller
@@ -11,5 +12,14 @@ class TodoController extends Controller
     {
         $todos = Todo::all();
         return response()->json($todos, Response::HTTP_OK);
+    }
+
+    public function store(Request $request)
+    {
+        $todo = Todo::create([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+        return response()->json($todo, Response::HTTP_CREATED);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\TodoController;
 
 
 Route::get('/v1/todos', [TodoController::class, 'index']);
+Route::post('/v1/todos', [TodoController::class, 'store']);
