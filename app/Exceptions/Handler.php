f6306d198c29cb01556fc4af2c42e6f381d226a1
Mojtaba Michael <mojimich2015@gmail.com>
2022-06-30 14:33:10 +0430

    It has been added json response handler for exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,19 @@
 
 namespace App\Exceptions;
 
+use App\Traits\CustomApiResponser;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 class Handler extends ExceptionHandler
 {
+    use CustomApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -35,7 +43,33 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
         });
+        $this->renderable(function (Throwable $e, $request) {
+            return $this->handleExceptions($e, $request);
+        });
+    }
+
+    public function handleExceptions(Throwable $e, $request)
+    {
+        //401 Unauthorized
+        if ($e instanceof AuthenticationException && $request->expectsJson()) {
+            return $this->errorResponse([], 'Authentication failure', Response::HTTP_UNAUTHORIZED);
+        }
+        //404 Not Found
+        if ($e instanceof NotFoundHttpException && $request->expectsJson()) {
+            return $this->errorResponse([], 'Not found', Response::HTTP_NOT_FOUND);
+        }
+        //405 Method Not Allowed
+        if ($e instanceof MethodNotAllowedException && $request->expectsJson()) {
+            return $this->errorResponse([], 'Method not allowed exception', Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+        //400 & 422 Unprocessable Entity
+        if ($e instanceof ValidationException && $request->expectsJson()) {
+            return $this->errorResponse(['errors' => $e->validator->getMessageBag()], 'Incorrect data', $e->status);
+        }
+        //429 Too Many Requests
+        if ($e instanceof ThrottleRequestsException && $request->expectsJson()) {
+            return $this->errorResponse([], 'Too many requests', Response::HTTP_TOO_MANY_REQUESTS);
+        }
     }
 }
